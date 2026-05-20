e8cc8bd9265c2463b7baf8bc3a1fd6eb1850eb3f
vihangagee-rathnayake <vihangageerathnayake@gmail.com>
2026-05-20 12:06:25 +0530

    update

--- gembackend/src/main/java/com/srilankagem/gembackend/gem/repository/GemStoneRepository.java
+++ gembackend/src/main/java/com/srilankagem/gembackend/gem/repository/GemStoneRepository.java
@@ -1,6 +1,8 @@
 package com.srilankagem.gembackend.gem.repository;
 
 import com.srilankagem.gembackend.gem.models.GemStone;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
